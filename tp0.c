60764f9e5facfbead409850da0963370297455b2
MarkoVrljicak <marcos.vrljicak@gmail.com>
2016-09-07 18:34:59 -0300

    Entrada y verificacion

--- tp0.c
+++ tp0.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdbool.h>
 #include <math.h>
+#include <ctype.h>
+#include <limits.h>
 
 #define COMANDO_R "-r"
 #define COMANDO_C "-c"
@@ -11,6 +13,18 @@
 #define COMANDO_H "-H"
 #define COMANDO_OUTPUT "-o"
 #define ERROR "e"
+
+#define PARAMETRO_DEF_LEN 15
+#define COMPLEJO_MAX_LEN 20
+
+#define RESOLUCION_ANCHO_DEFAULT 640
+#define RESOLUCION_ALTO_DEFAULT 480
+#define CENTRO_REAL_DEFAULT 0
+#define CENTRO_IMAGINARIO_DEFAULT 0
+#define C_REAL_DEFAULT 0
+#define C_IMAGINARIO_DEFAULT 0
+#define DIMENSION_ANCHO_DEFAULT 4
+#define DIMENSION_ALTO_DEFAULT 4
 
 #define HEADER_IMAGEN "P2"
 #define MAXIMA_INTENSIDAD_PIXEL 255
@@ -22,66 +36,229 @@
 typedef struct NumeroComplejo {
 	double parteReal;
 	double parteImaginaria;
-}NumeroComplejo;
+} NumeroComplejo;
 
 typedef struct Resolucion {
 	int ancho;
 	int alto;
-}Resolucion;
+} Resolucion;
 
 typedef struct Dimension {
 	float ancho;
 	float alto;
-}Dimension;
-
-typedef struct Comando {
-	char* tipo;
-	char* dato;
-}Comando;
-
-typedef struct Commandos {
-	Comando* *datos;
-	int cantidadComandos;
-}Comandos;
+} Dimension;
 
 typedef struct ConfiguracionConjunto {
 	Resolucion resolucion; // Resolución de la imagen a generar
 	Dimension dimension; // Tamaño de la porción de plano complejo
 	NumeroComplejo centro;
-	NumeroComplejo parametroC;
-	char* nombreImagen;
-	bool salidaImagen;
-}ConfiguracionConjunto;
-
+	NumeroComplejo c;
+	char nombreImagen[PARAMETRO_DEF_LEN];
+	bool salidaEstandar;
+} ConfiguracionConjunto;
 
 /* ******************************************************************
- *                        Función de Lectura
+ *                          Manual
  * *****************************************************************/
 
-Comandos* leerDatos(int argc, char const *argv[]) {
-
-	Comandos* comandos = malloc(sizeof(Comandos));
-
-	if (!comandos) return NULL;
-
-	/*Implementar la validación de los datos de entrada*/
-
-	return NULL;
+void printUso() {
+	printf("Usage: ./tp0 [options]\n"
+				   "-r [resolution]     specify resolution\n"
+				   "-c [complex number] specify center\n"
+				   "-C [complex number] specify c parameter\n"
+				   "-W [width]          specify width of output\n"
+				   "-H [hight]          specify height of output\n"
+				   "-o [filename].pgm   specify file name, or type '-' for standard output\n");
 }
 
 /* ******************************************************************
- *     Configuración de las dimensiones de la imagen a generar
+ *                        Validación
  * *****************************************************************/
 
-ConfiguracionConjunto* configurarConjunto(Comandos* comandos) {
+bool isNumber(char* str, size_t len){
+	for (int i=0; i < len; i++)
+		if (!isdigit(str[i])) {
+			return false;
+		}
+	return true;
+}
 
+bool resolucionValida(Resolucion resolucion){
+	return (resolucion.ancho > 0 && resolucion.alto > 0);
+}
+
+bool nombreValido(char* nombre){
+    bool valido;
+    if (strcmp(nombre, "-") == 0) valido = true;
+    else {
+        const char* punto = strrchr(nombre, '.');
+        if (!punto || punto == nombre ||
+                strcmp(punto, ".pgm") != 0) valido = false;
+        else valido = true;
+    }
+    if (!valido) printf("fatal: cannot open output file.\n");
+    return valido;
+}
+
+bool complejoValido(NumeroComplejo complejo, char* errorMsg){
+    bool valido = complejo.parteReal != INT_MAX;
+    if (!valido) puts(errorMsg);
+    return valido;
+}
+
+/* ******************************************************************
+ *                         Lectura
+ * *****************************************************************/
+
+NumeroComplejo numeroComplejoInvalido(){
+    NumeroComplejo invalido;
+    invalido.parteReal = INT_MAX;
+    return invalido;
+}
+
+NumeroComplejo stringANumeroComplejo(const char* complejoStr){
+	NumeroComplejo complejo;
+    //Valido
+    if (!(complejoStr && *complejoStr &&
+            complejoStr[strlen(complejoStr) - 1] == 'i')){
+        return numeroComplejoInvalido();
+    }
+
+	//Homogeneizo entrada
+	char str[COMPLEJO_MAX_LEN];
+	if (complejoStr[0] != '+' && complejoStr[0] != '-') {
+		strcpy(str, "+");
+        strcat(str, complejoStr);
+	} else {
+        strcpy(str, complejoStr);
+    }
+
+	int signoReal = 1;
+	int signoImaginario = 1;
+	int i = 0, cuentaMas = 0, cuentaMenos = 0;
+	for (i = 0; str[i]; i++) {
+		cuentaMas += (str[i] == '+');
+		cuentaMenos += (str[i] == '-');
+	}
+
+    if (cuentaMas + cuentaMenos != 2) {
+        return numeroComplejoInvalido();
+    }
+	if (cuentaMas == 0) {
+		signoImaginario = -1;
+		signoReal = -1;
+	} else if (cuentaMas == 1){
+		if (str[0] == '+') signoImaginario = -1;
+		else signoReal = -1;
+	}
+
+	//Parseo
+	char* tmp;
+	tmp = strtok(str, " +-i");
+	if (!isNumber(tmp, strlen(tmp))) return numeroComplejoInvalido();
+	else {
+        complejo.parteReal = signoReal*atof(tmp);
+    }
+	tmp = strtok(NULL, " +-i");
+	if (!isNumber(tmp, strlen(tmp))) return numeroComplejoInvalido();
+	else {
+        complejo.parteImaginaria = signoImaginario*atof(tmp);
+    }
+
+	return complejo;
+}
+
+ConfiguracionConjunto* leerDatos(int argc, char const *argv[]) {
+	//Configuracion default
 	ConfiguracionConjunto* configuracion = malloc(sizeof(ConfiguracionConjunto));
+	Resolucion resolucion;
+	resolucion.ancho = RESOLUCION_ANCHO_DEFAULT;
+	resolucion.alto = RESOLUCION_ALTO_DEFAULT;
+	configuracion->resolucion = resolucion;
+	NumeroComplejo centro;
+	centro.parteReal = CENTRO_REAL_DEFAULT;
+	centro.parteImaginaria = CENTRO_IMAGINARIO_DEFAULT;
+	configuracion->centro = centro;
+	NumeroComplejo c;
+	c.parteReal = C_REAL_DEFAULT;
+	c.parteImaginaria = C_IMAGINARIO_DEFAULT;
+	configuracion->c = c;
+	Dimension dimension;
+	dimension.ancho = DIMENSION_ANCHO_DEFAULT;
+	dimension.alto = DIMENSION_ALTO_DEFAULT;
+	configuracion->dimension = dimension;
+	configuracion->salidaEstandar = false;
 
-	if (!configuracion) return NULL;
+	//Configuracion custom
+    int i;
+	for (i = 1; i < argc; i++){
+		if (strcmp(argv[i], COMANDO_R) == 0) {
+			i++;
+			if (i < argc) {
+				char parametro[PARAMETRO_DEF_LEN];
+				strcpy(parametro, argv[i]);
+				char* valor;
+				valor = strtok(parametro, "x");
+				if (!isNumber(valor, strlen(valor))) break;
+				configuracion->resolucion.ancho = atoi(valor);
+				valor = strtok(NULL, "x");
+				if (!isNumber(valor, strlen(valor))) break;
+				configuracion->resolucion.alto = atoi(valor);
+			}
+		} else if (strcmp(argv[i], COMANDO_C) == 0) {
+			i++;
+			if (i < argc){
+				configuracion->centro = stringANumeroComplejo(argv[i]);
+                if (!complejoValido(configuracion->centro,
+                                    "fatal: invalid center specification.")){
+                    free(configuracion);
+                    return NULL;
+                }
+			}
+		} else if (strcmp(argv[i], COMANDO_PARAMETRO_C) == 0) {
+			i++;
+			if (i < argc){
+				configuracion->c = stringANumeroComplejo(argv[i]);
+                if (!complejoValido(configuracion->c,
+                                    "fatal: invalid c parameter specification.")){
+                    free(configuracion);
+                    return NULL;
+                }
+			}
+		} else if (strcmp(argv[i], COMANDO_W) == 0) {
+			i++;
+			if (i < argc){
+				if (!isdigit(argv[i])) break;
+				else configuracion->dimension.ancho = atof(argv[i]);
+			}
+		} else if (strcmp(argv[i], COMANDO_H) == 0) {
+			i++;
+			if (i < argc){
+				if (!isdigit(argv[i])) break;
+				else configuracion->dimension.alto = atof(argv[i]);
+			}
+		} else if (strcmp(argv[i], COMANDO_OUTPUT) == 0) {
+			i++;
+			if (i < argc) {
+				strcpy(configuracion->nombreImagen, argv[i]);
+                if (!nombreValido(configuracion->nombreImagen)) {
+                    free(configuracion);
+                    return NULL;
+                }
+			} else break;
+		} else break;
+	}
+	if (*configuracion->nombreImagen == '-'){
+		configuracion->salidaEstandar = true;
+	}
 
-	for (int i = 0; i < comandos->cantidadComandos; i++) {
-
-		/*A partir de los comandos ya guardados, obtener la información para la configuración*/
+    //Errores de uso
+	if (argc == 1 || argc != i ||
+            !resolucionValida(configuracion->resolucion) ||
+            !nombreValido(configuracion->nombreImagen)){
+		free(configuracion);
+		printUso();
+		return NULL;
 	}
 
 	return configuracion;
@@ -118,103 +295,69 @@ double moduloAlCuadrado(NumeroComplejo unNumero){
  * *****************************************************************/
 
 unsigned char calcularBrillo(NumeroComplejo numeroComplejo, NumeroComplejo c){
-	NumeroComplejo iComplejo = numeroComplejo;
-	unsigned char i = 0;
-	while(i < 255 && moduloAlCuadrado(iComplejo) <= 4){
-		iComplejo = sumar(multiplicar(iComplejo, iComplejo),c);
-		i++;
-	}
-	return i;
+    NumeroComplejo iComplejo = numeroComplejo;
+    unsigned char i = 0;
+    while(i < 255 && moduloAlCuadrado(iComplejo) <= 4){
+        iComplejo = sumar(multiplicar(iComplejo, iComplejo),c);
+        i++;
+    }
+    return i;
 }
 
 void simularConjunto(ConfiguracionConjunto* configuracion) {
 
-	FILE* imagen;
+    FILE* imagen;
 
-	int anchoRes = configuracion->resolucion.ancho;
-	int altoRes = configuracion->resolucion.alto;
+    int anchoRes = configuracion->resolucion.ancho;
+    int altoRes = configuracion->resolucion.alto;
 
-	if (configuracion->salidaImagen) {
+    if (!configuracion->salidaEstandar) {
 
-		imagen = fopen(configuracion->nombreImagen, "w");
+        imagen = fopen(configuracion->nombreImagen, "w");
 
-		fprintf(imagen, "%s\n", HEADER_IMAGEN);
-		fprintf(imagen, "%d %d\n", anchoRes, altoRes);
-		fprintf(imagen, "%d\n", MAXIMA_INTENSIDAD_PIXEL);
+        fprintf(imagen, "%s\n", HEADER_IMAGEN);
+        fprintf(imagen, "%d %d\n", anchoRes, altoRes);
+        fprintf(imagen, "%d\n", MAXIMA_INTENSIDAD_PIXEL);
 
-	} else {
+    } else {
 
-		printf("%s\n", HEADER_IMAGEN);
-		printf("%d %d\n", anchoRes, altoRes);
-		printf("%d\n", MAXIMA_INTENSIDAD_PIXEL);
-	}
+        printf("%s\n", HEADER_IMAGEN);
+        printf("%d %d\n", anchoRes, altoRes);
+        printf("%d\n", MAXIMA_INTENSIDAD_PIXEL);
+    }
 
-	unsigned char brillo;
-	NumeroComplejo unNumero;
-	double incrementoParteReal, incrementoParteImaginaria, anchoPixel, altoPixel;
+    unsigned char brillo;
+    NumeroComplejo unNumero;
+    double incrementoParteReal, incrementoParteImaginaria, anchoPixel, altoPixel;
 
-	NumeroComplejo centro = configuracion->centro;
+    NumeroComplejo centro = configuracion->centro;
 
-	incrementoParteReal = configuracion->dimension.ancho / anchoRes;
-	incrementoParteImaginaria = configuracion->dimension.alto / altoRes;
-	anchoPixel = anchoRes/configuracion->dimension.ancho;
-	altoPixel = altoRes/configuracion->dimension.alto;
+    incrementoParteReal = configuracion->dimension.ancho / anchoRes;
+    incrementoParteImaginaria = configuracion->dimension.alto / altoRes;
+    anchoPixel = anchoRes/configuracion->dimension.ancho;
+    altoPixel = altoRes/configuracion->dimension.alto;
 
-	for(double i = -anchoRes/2; i <= anchoRes/2; i++){
-		
-		for(double j = -altoRes/2; j <= altoRes/2; j++){
-			unNumero.parteReal = centro.parteReal + i*incrementoParteReal + anchoPixel;
-			unNumero.parteImaginaria = centro.parteImaginaria + j*incrementoParteImaginaria + altoPixel;
-			brillo = calcularBrillo(unNumero, configuracion->parametroC);
-			
-			if (configuracion->salidaImagen)
-				fprintf(imagen, "%d ", brillo);
-			else
-				printf("%d ", brillo);
-		}
+    for(double i = -anchoRes/2; i <= anchoRes/2; i++){
 
-		if (configuracion->salidaImagen)
-			fprintf(imagen, "%s\n", "");
-		else
-			printf("\n");
-	}
+        for(double j = -altoRes/2; j <= altoRes/2; j++){
+            unNumero.parteReal = centro.parteReal + i*incrementoParteReal + anchoPixel;
+            unNumero.parteImaginaria = centro.parteImaginaria + j*incrementoParteImaginaria + altoPixel;
+            brillo = calcularBrillo(unNumero, configuracion->c);
 
-	if (configuracion->salidaImagen)
-		fclose(imagen);
-}
+            if (!configuracion->salidaEstandar)
+                fprintf(imagen, "%d ", brillo);
+            else
+                printf("%d ", brillo);
+        }
 
-/* ******************************************************************
- *                 Funciones de destrucción de datos
- * *****************************************************************/
+        if (!configuracion->salidaEstandar)
+            fprintf(imagen, "%s\n", "");
+        else
+            printf("\n");
+    }
 
-void destruirComandos(Comandos* comandos) {
-
-	Comando* *datos = comandos->datos;
-
-	for (int i = 0; i < comandos->cantidadComandos; i++) {
-
-		Comando* comando = datos[i];
-		free(comando->tipo);
-		free(comando->dato);
-		free(comando);
-	}
-
-	free(datos);
-
-	free(comandos);
-}
-
-void destruirConfiguracionConjunto(ConfiguracionConjunto* configuracion) {
-
-	free(configuracion->nombreImagen);
-
-	free(configuracion);
-}
-
-void destruirDatos(Comandos* comandos, ConfiguracionConjunto* configuracion) {
-
-	destruirComandos(comandos);
-	destruirConfiguracionConjunto(configuracion);
+    if (!configuracion->salidaEstandar)
+        fclose(imagen);
 }
 
 /* ******************************************************************
@@ -222,31 +365,12 @@ void destruirDatos(Comandos* comandos, ConfiguracionConjunto* configuracion) {
  * *****************************************************************/
 
 void simulacionJulia(int argc, char const *argv[]) {
-
-	Comandos* comandos = leerDatos(argc, argv);
-
-	Comando* *datos = comandos->datos;
-
-	Comando* comando = datos[0];
-
-	if (strcmp(comando->tipo, ERROR) == 0) {
-
-		printf("%s\n", comando->dato);
-		destruirComandos(comandos);
-
-	} else {
-
-		ConfiguracionConjunto* configuracion = configurarConjunto(comandos);
-
-		simularConjunto(configuracion);
-
-		destruirDatos(comandos, configuracion);
-	}
+	ConfiguracionConjunto* configuracion = leerDatos(argc, argv);
+    if (configuracion) simularConjunto(configuracion);
+    free(configuracion);
 }
 
-int main(int argc, char const *argv[]) {
-	
+int main(int argc, char const *argv[]) {	
 	simulacionJulia(argc, argv);
-
 	return 0;
 }
